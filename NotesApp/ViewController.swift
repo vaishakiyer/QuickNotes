03bdefd115c4b32765756a14746ce85720c51269
Vaishak.Iyer <vaishak.iyer@tests-macbook-air.local>
2018-04-13 16:17:36 +0530

    Initial Commit

---
+++ NotesApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NotesApp
+//
+//  Created by Vaishak.Iyer on 13/04/18.
+//  Copyright © 2018 Vaishak.Iyer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
